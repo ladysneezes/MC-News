1b3ddf52cc662bf1cf034725a20b9baac0e036ba
ladysneezes <franktmike@gmail.com>
2019-06-28 10:56:18 +0100

    removed handle function

--- src/Components/DeleteButton.js
+++ src/Components/DeleteButton.js
@@ -2,17 +2,12 @@ import React from "react";
 
 const DeleteButton = props => {
   const { comment_id, removeAComment, user, author } = props;
-  const handleDelete = comment_id => {
-    removeAComment(comment_id);
-  };
   return (
     <>
       <button
         className="waves-effect waves-light btn"
         disabled={user !== author}
-        onClick={() => {
-          handleDelete(comment_id);
-        }}
+        onClick={() => removeAComment(comment_id)}
       >
         Delete
       </button>
