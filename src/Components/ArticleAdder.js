1cdf3b77cacdfac0a13d8109a80c8d6d41d1a4e4
ladysneezes <franktmike@gmail.com>
2019-07-01 15:04:48 +0100

    styling added

--- src/Components/ArticleAdder.js
+++ src/Components/ArticleAdder.js
@@ -2,46 +2,83 @@ import React, { Component } from "react";
 import * as api from "../api";
 
 class ArticleAdder extends Component {
-  state = { title: "", body: "", topic: "" };
+  state = { title: "", body: "", topic: "", topics: [] };
   render() {
-    const { title, body, topic } = this.state;
+    const { title, body, topics } = this.state;
     return (
-      // add topics as dropdown
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Title:
-          <input
-            type="text"
-            name="title"
-            value={title}
-            onchange={this.handleChange}
-          />
-        </label>
-        <label>
-          Body:
-          <input
-            type="text"
-            name="body"
-            value={body}
-            onChange={this.handleChange}
-          />
-        </label>
-        <button>Submit Article.</button>
-      </form>
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Title:
+            <input
+              required
+              type="text"
+              name="title"
+              value={title}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Body:
+            <input
+              required
+              type="text"
+              name="body"
+              value={body}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Topic:
+            {topics.map(topic => (
+              <p>
+                <label>
+                  <input
+                    name="topics_list"
+                    type="radio"
+                    value={topic.slug}
+                    required
+                  />
+                  <span>{topic.slug}</span>
+                </label>
+              </p>
+            ))}
+          </label>
+          <button
+            class="btn waves-effect waves-light"
+            type="submit"
+            name="action"
+          >
+            Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </form>
+      </div>
     );
   }
+
   handleSubmit = event => {
     event.preventDefault();
-    api.postArticle(username, title, body).then(article => {
+    api.postArticle({ ...this.state, ...this.props }).then(article => {
       //needs topic as well
       this.setState({ title: "", body: "", topic: "" });
-      this.props.navigate(`article/${article_id}`);
+      this.props.navigate(`/articles/${article.article_id}`);
     });
   };
 
   handleChange = event => {
-    const { name, value } = this.event.target;
+    const { name, value } = event.target;
     this.setState({ [name]: value });
+  };
+
+  componentDidMount = () => {
+    api
+      .getTopics()
+      .then(res => {
+        this.setState({ topics: res.topics });
+        return res.topics[0];
+      })
+      .then(topic => this.setState({ topic: topic.slug }));
   };
 }
 
