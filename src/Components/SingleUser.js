12e8a1fb572db4f176153d0a01de52206e5aee3a
ladysneezes <franktmike@gmail.com>
2019-07-07 17:13:31 +0100

    add error page

--- src/Components/SingleUser.js
+++ src/Components/SingleUser.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 import * as api from "../api";
 import ArticleCard from "./ArticleCard";
 import UserCard from "./UserCard";
+import Error from "./Error";
 
 class SingleUser extends Component {
   state = { articles: [], error: null, user: {} };
   render() {
     const { articles, user, error } = this.state;
+
+    if (error) {
+      return <Error error={error} />;
+    }
+
     return (
       <>
-        <UserCard user={this.state.user} />
+        <UserCard user={user} />
         <ul>
           {articles.map(article => (
             <ArticleCard key={article.article_id} article={article} />
