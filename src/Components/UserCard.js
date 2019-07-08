0fd34fb363afdc1bf10b0046fe809b437e4f3f40
ladysneezes <franktmike@gmail.com>
2019-07-08 18:10:16 +0100

    style usercard

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,17 +1,56 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const UserCard = props => {
-  const { username, name, avatar_url } = props.user;
-  return (
-    <>
-      <p>
-        Username: {username}
-        <br />
-        Name: {name}
-        <br />
-        <img src={avatar_url} alt={`${username}'s avatar`} />
-      </p>
-    </>
+  const { username, name, avatar_url, singleUser } = props.user;
+  return singleUser ? (
+    <div className="row">
+      <div className="col s12 m10 offset-m1">
+        <div className="card-panel grey lighten-5 z-depth-1">
+          <div className="row valign-wrapper">
+            <div className="col s2 m2">
+              <img
+                src={avatar_url}
+                alt="avatar"
+                className="circle responsive-img"
+              />
+            </div>
+            <div className="col s10">
+              <p className="black-text">
+                Username: {username}
+                <br />
+                Name: {name}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="row">
+      <div className="col s12 m10 offset-m1">
+        <Link to={`/users/${username}`}>
+          <div className="card-panel grey lighten-5 z-depth-1">
+            <div className="row valign-wrapper">
+              <div className="col s2 m2">
+                <img
+                  src={avatar_url}
+                  alt="avatar"
+                  className="circle responsive-img"
+                />
+              </div>
+              <div className="col s6 m6">
+                <p className="black-text">
+                  Username: {username}
+                  <br />
+                  Name: {name}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 };
 
