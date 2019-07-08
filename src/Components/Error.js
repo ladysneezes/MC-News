6a96e9ff4a8a76d0bcf5ca792ac524968523907a
ladysneezes <franktmike@gmail.com>
2019-07-08 08:49:57 +0100

    handle custom errors

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,8 +2,18 @@ import React from "react";
 
 const Error = props => {
   const { error } = props;
-  if (error.message) return <p>{error.message}</p>;
-  else return <p>Error</p>;
+
+  if (error && error.response.data.msg) {
+    return (
+      <p>
+        {error.response.status}: {error.response.data.msg}
+      </p>
+    );
+  } else if (error && error.message) {
+    return <p>{error.message}</p>;
+  } else {
+    return <p>404 Page not found</p>;
+  }
 };
 
 export default Error;
