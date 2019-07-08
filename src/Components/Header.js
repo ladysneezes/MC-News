7934fceb8750b9f565d7a994c53c41e06227c062
ladysneezes <franktmike@gmail.com>
2019-07-08 11:57:38 +0100

    fixed navbar styling added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,25 +4,44 @@ import { Link } from "@reach/router";
 const Header = props => {
   return (
     <header>
-      <nav className="navButtons">
-        <div className="nav-wrapper">
-          <h6 className="right">User: {props.user}</h6>
-          <ul id="nav-mobile" className="left">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-            <li>
-              <Link to="/articles">Add an Article</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div className="navbar-fixed">
+        <nav className="navButtons">
+          <div className="nav-wrapper">
+            {/* <button className="right">User: {props.user}</button> */}
+            {/* <a href="#" data-target="mobile-demo" className="sidenav-trigger"> */}
+            {/* <i className="material-icons">menu</i>
+            </a> */}
+            <ul id="nav-mobile" className="left">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/topics">Topics</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+              <li>
+                <Link to="/articles">Add an Article</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      {/* <ul className="sidenav" id="mobile-demo">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/articles">Add an Article</Link>
+        </li>
+      </ul> */}
     </header>
   );
 };
